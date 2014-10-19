d3b9f605a0fbcc82fb47cb6ad16dbec8909ccfff
nishanths <nishanth.gerrard@gmail.com>
2014-10-19 04:22:09 -0500

    add YouTube, brighten lights when video tab removed

--- ext/src/bg/background.js
+++ ext/src/bg/background.js
@@ -24,46 +24,63 @@ chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
     }
 });
 
-chrome.windows.getAll(null, function(w) {
-	console.log(w);
-	for (var i = 0; i < w.length; i++) {
-		// console.log(i);
-		var windowState = w[i].state;
-		// console.log(windowState);
-		console.log("Window id: " + w[i].id + " (" + w[i].state + ")");
+// var currentVideoTabsIds = [];
+// Big picture: Tells the hardware to turn lights off depending on Vimeo's full screen
+chrome.webNavigation.onCommitted.addListener(function(e) {
+    	// Look at all tabs in each Chrome window
+    	// If one of the tabs is Vimeo full screen, POST to dim lights
 
-		// query with Window.id
-		chrome.tabs.query({windowId: w[i].id}, function(t){
-			// for the tab-less windows such as the Inspector
-			if (typeof t === "undefined") { 
-				console.log("Please bitch about it");
-			}
+    	console.log("I'm up because a vimeo page was loaded.");
+      	chrome.windows.getAll(null, function(w) {
+			for (var i = 0; i < w.length; i++) {
+				var windowState = w[i].state;
+				console.log("Window id: " + w[i].id + " (" + w[i].state + ")");
 
-			for (var j = 0; j < t.length; j++) {
-				if (windowState == "fullscreen") {
-					console.log(t[j].url);
-					if (/vimeo.com/.test(t[j].url)) {
-						console.log("tab id " + t[j].id + " with a vimeo url has the status:");
-						console.log(windowState);
-						// Call the lights dimmer here
+				// query with Window.id
+				chrome.tabs.query({windowId: w[i].id}, function(t){
+					// for the tab-less windows such as the Inspector
+					if (typeof t === "undefined") { 
+						console.log("Please bitch about it");
 					}
-				}
+
+					for (var j = 0; j < t.length; j++) {
+						// console.log(windowState);
+						// console.log(windowState == "fullscreen");	
+						// if (windowState == "fullscreen") {
+							console.log(t[j].url);
+							if (/vimeo.com/.test(t[j].url)) {
+								console.log("tab id " + t[j].id + " with a vimeo url has the status:");
+								console.log(windowState);
+								var tabID = t[j].id;
+
+								// Make a POST to
+								// Call the lights dimmer here
+								console.log("POSTing");
+								httpPost("/dim-lights", "Dim");
+
+								// currentVideoTabs.push(t[j].id);
+
+								// Add listener for removal
+								chrome.tabs.onRemoved.addListener(function(tabID) {
+									console.log("Video tab with id:" + tabID + " was removed");
+									console.log("Now telling lights to come back on.");
+									httpPost("/brighten-lights", "Brighten");	
+								}); 
+							}
+						// }
+					}
+				});
 			}
 		});
-	}
-});
+}, { url: [ {urlMatches: 'vimeo.com'}, {urlMatches: 'youtube.com'} ] });
 
-var dist_callback_string = "?callback=useDistanceData";
 
+// var dist_callback_string = "?callback=useDistanceData"; // not using callbacks 
 
 var currentlyAway = false;
 
-// console.log(res);
-// console.log(typeof res);
-// var farAwayFromComputer = res["farAwayFromComputer"];
-
 setInterval(function() {
-	console.log("Polling");
+	console.log("Polling the Log out stuff");
 
 	var res = httpGet("http://localhost:3000/distance-sensors");
 	var parsed = JSON.parse(res);
@@ -96,6 +113,13 @@ function httpGet(theUrl) {
     xmlHttp.open( "GET", theUrl, false );
     xmlHttp.send( null );
     return xmlHttp.responseText;
+}
+
+function httpPost(path, message) {
+  var client = new XMLHttpRequest();
+  client.open("POST", "http://localhost:3000" + path);
+  client.setRequestHeader("Content-Type", "text/plain;charset=UTF-8");
+  client.send(message);
 }
 
 function useDistanceData(unparsed_json_resp) {
